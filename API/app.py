c22118784e1fec5b5f14395d8e64d3cfe0dcb9ae
Joan Sebastian Hernandez Romero <81491007+Jsebashr@users.noreply.github.com>
2025-08-24 18:38:10 -0500

    Update app.py

--- API/app.py
+++ API/app.py
@@ -14,3 +14,8 @@ def get_familia():
 def get_superheroes():
     rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
     return rows
+
+app.get("/superMavel")
+def get_superheroes():
+    rows = ["Spiderman", "Iron man", "Hulk, "Wolwerine", "Ghost Rider", "Namor", "Black Panter", "Daredevil"]
+    return rows
